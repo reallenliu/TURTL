36c2b22c12108c4c45427a2bde834c7d9e4ae84b
Jason Vorderbruggen <74619906+JVorderbruggen@users.noreply.github.com>
2020-11-19 13:41:03 -0600

    Update L1_RGBLED.py

--- L1_RGBLED.py
+++ L1_RGBLED.py
@@ -3,30 +3,42 @@
 import L1_gpio as gpio
 
 #define variables
-State = 0;
-gpioport = 0;   #gpio port 0 needs to be configured to output on Beaglebone
+State = 0
+gpioport = 0  #gpio port 0 needs to be configured to output on Beaglebone
 
 #functions
 def LEDState(State, gpioport):      #takes the current step and gpioport and outputs the appropriate stepper values 
     if state == 1:                  #Seeking state            
-        gpio.write(gpioport, 0, 1)
+        gpio.write(gpioport, 0, 0)
         gpio.write(gpioport, 1, 0)
         gpio.write(gpioport, 2, 0)
-        gpio.write(gpioport, 3, 0)
-    elif state == 2:                #Firing state
-        gpio.write(gpioport, 0, 0)
-        gpio.write(gpioport, 1, 1)
-        gpio.write(gpioport, 2, 0)
-        gpio.write(gpioport, 3, 0)
-    elif state == 3:                #Error State
+    elif state == 2:                #Found Target
         gpio.write(gpioport, 0, 0)
         gpio.write(gpioport, 1, 0)
         gpio.write(gpioport, 2, 1)
-        gpio.write(gpioport, 3, 0)
-    elif state == 4:                #Manual Control State
+    elif state == 3:                #Shooting target
         gpio.write(gpioport, 0, 0)
+        gpio.write(gpioport, 1, 1)
+        gpio.write(gpioport, 2, 1)
+    elif state == 4:                #Turning Left
+        gpio.write(gpioport, 0, 0)
+        gpio.write(gpioport, 1, 1)
+        gpio.write(gpioport, 2, 1)
+    elif state == 5:                #Turning Right
+        gpio.write(gpioport, 0, 1)
         gpio.write(gpioport, 1, 0)
         gpio.write(gpioport, 2, 0)
-        gpio.write(gpioport, 3, 1)
+    elif state == 6:                #Mission Complete
+        gpio.write(gpioport, 0, 1)
+        gpio.write(gpioport, 1, 0)
+        gpio.write(gpioport, 2, 1)
+    elif state == 7:                #Failure
+        gpio.write(gpioport, 0, 1)
+        gpio.write(gpioport, 1, 1)
+        gpio.write(gpioport, 2, 0)
+    elif state == 8:                #Manual Control Mode
+        gpio.write(gpioport, 0, 1)
+        gpio.write(gpioport, 1, 1)
+        gpio.write(gpioport, 2, 1)
     else:
         print("ERROR: TURTL State not valid")
